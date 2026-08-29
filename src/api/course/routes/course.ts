1d9338b9f4969768af8d75f860f410743da60ea5
Riddhi73 <riddhimandebnath4260@gmail.com>
2026-08-29 12:21:42 +0600

    Add permanent API files for course content-types

--- src/api/course/routes/course.ts
+++ src/api/course/routes/course.ts
@@ -2,6 +2,54 @@
  * course router
  */
 
-import { factories } from '@strapi/strapi';
+"use strict";
 
-export default factories.createCoreRouter('api::course.course');
+export default {
+  routes: [
+    {
+      method: "GET",
+      path: "/courses",
+      handler: "course.find",
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: "GET",
+      path: "/courses/:id",
+      handler: "course.findOne",
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: "POST",
+      path: "/courses",
+      handler: "course.create",
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: "PUT",
+      path: "/courses/:id",
+      handler: "course.update",
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: "DELETE",
+      path: "/courses/:id",
+      handler: "course.delete",
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+  ],
+};
